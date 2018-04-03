f153edaff677c5305bba67be57cd2d62afffd335
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-04-03 12:18:46 -0300

    Improve test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,23 +1,25 @@
-# from datetime import datetime
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-#
-# from twitter.models import Tweet
-#
-# User = get_user_model()
-#
-#
-# class TwitterDataTestCase(TestCase):
-#     def test_tweet_info(self):
-#         self.assertEqual(Tweet.objects.count(), 2)
-#         evs_tweet = Tweet.objects.get(content='checking out twttr')
-#         # Test created, content and user are not null
-
-
 import pytest
+from datetime import datetime
 
-from twitter.models import Tweet
+from django.utils.timezone import utc
+
+from twitter.models import Tweet, User
 from base import base_twitter_fixture, base_authenticated_fixture
+
+
+@pytest.mark.django_db(transaction=False)
+def test_tweets_are_migrated():
+    assert Tweet.objects.count() == 2
+    jack = User.objects.get(username='jack')
+    ev = User.objects.get(username='ev')
+    jacks_tweet = Tweet.objects.get(content='just setting up my twttr')
+    evs_tweet = Tweet.objects.get(content='checking out twttr')
+
+    assert jacks_tweet.created, datetime(2006, 3, 21, 2, 50, tzinfo=utc)
+    assert evs_tweet.created, datetime(2006, 3, 21, 5, 51, tzinfo=utc)
+
+    assert jacks_tweet.user == jack
+    assert evs_tweet.user == ev
 
 
 def test_tweet_info(base_twitter_fixture):
