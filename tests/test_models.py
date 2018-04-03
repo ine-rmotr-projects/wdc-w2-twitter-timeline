4ea62d8f889fca965854def4168bd1dc19736047
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-04-03 12:14:23 -0300

    Refactor tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,28 @@
+import pytest
 from datetime import datetime
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+
 from django.utils.timezone import utc
 
-from twitter.models import Tweet
+from twitter.models import Tweet, User
+from base import base_twitter_fixture, base_authenticated_fixture
 
-User = get_user_model()
+
+def test_tweets_are_migrated():
+    assert Tweet.objects.count() == 2
+    jack = User.objects.get(username='jack')
+    ev = User.objects.get(username='ev')
+    jacks_tweet = Tweet.objects.get(content='just setting up my twttr')
+    evs_tweet = Tweet.objects.get(content='checking out twttr')
+
+    assert jacks_tweet.created, datetime(2006, 3, 21, 2, 50, tzinfo=utc)
+    assert evs_tweet.created, datetime(2006, 3, 21, 5, 51, tzinfo=utc)
+
+    assert jacks_tweet.user == jack
+    assert evs_tweet.user == ev
 
 
-class TwitterDataTestCase(TestCase):
-    def test_tweets_are_migrated(self):
-        self.assertEqual(Tweet.objects.count(), 2)
-        jack = User.objects.get(username='jack')
-        ev = User.objects.get(username='ev')
-        jacks_tweet = Tweet.objects.get(content='just setting up my twttr')
-        evs_tweet = Tweet.objects.get(content='checking out twttr')
+def test_tweet_info(base_twitter_fixture):
+    Tweet.objects.create(user=base_twitter_fixture['jack'], content='Tweet Jack 1')
+    Tweet.objects.create(user=base_twitter_fixture['ev'], content='Tweet Evan 1')
 
-        self.assertEqual(
-            jacks_tweet.created, datetime(2006, 3, 21, 2, 50, tzinfo=utc))
-        self.assertEqual(
-            evs_tweet.created, datetime(2006, 3, 21, 5, 51, tzinfo=utc))
-
-        self.assertEqual(jacks_tweet.user, jack)
-        self.assertEqual(evs_tweet.user, ev)
+    assert Tweet.objects.count() == 2
