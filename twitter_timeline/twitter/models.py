b1f4580a85f584daecb9e3949e458b8686d86661
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-04-06 10:22:36 -0300

    Add ManyToMany relationships

--- twitter_timeline/twitter/models.py
+++ twitter_timeline/twitter/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from model_utils.models import TimeStampedModel
 
 
-class Tweet(models.Model):
+
+class Tweet(TimeStampedModel):
     class Meta:
         ordering = ['-created']
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like')
     content = models.CharField(max_length=140, blank=True)
-    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    @property
+    def count_likes(self):
+        return self.likes.count()
+
+    @property
+    def add_like_from_user(self, user):
+        self.likes.add(user)
+
+    @property
+    def remove_like_from_user(self, user):
+        self.likes.remove(user)
 
 
 class Relationship(models.Model):
     follower = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL, related_name='from_user',
+        on_delete=models.CASCADE
+    )
     following = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL, related_name='to_user',
+        on_delete=models.CASCADE
+    )
 
 
 class User(AbstractUser):
+
+    relationships = models.ManyToManyField(
+        "self", through='Relationship', symmetrical=False,
+        related_name='related_to'
+    )
 
     def follow(self, twitter_profile):
         try:
